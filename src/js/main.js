64eae42c471f1092d87b8d01cbe93ff3652f5a98
melnik <kivals.90@yandex.ru>
2020-07-09 17:07:54 +0300

    Add cats block

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-		ibg();
-	$('.intro-slider').slick({
-		arrows: false,
-		dots: true,
-		speed: 1000,
-		autoplay: true,
-		autoplaySpeed: 8000
-	});
+  ibg();
+  // $('.intro-slider').slick({
+  // 	arrows: false,
+  // 	dots: true,
+  // 	speed: 1000,
+  // 	autoplay: true,
+  // 	autoplaySpeed: 8000
+  // });
 
-	$('.slider-cats').slick({
-		dots: false,
-		slidesToShow: 4,
-		slidesToScroll: 4
-	});
-
+  $('.slider-cats').slick({
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
 });
 
 function ibg() {
   $.each($('.ibg'), function (index, val) {
     if ($(this).find('img').length > 0) {
-      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+      $(this).css('background-image', `url("${$(this).find('img').attr('src')}")`);
     }
   });
 }
